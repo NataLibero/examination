026229ba46917aea2a6ce7f4411178a31206d88c
unknown <jakushkolibero@gmail.com>
2018-07-02 16:32:15 +0200

    send-modal-form

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ window.addEventListener('DOMContentLoaded', function(){
 let popup_engineer_btn = document.querySelector('.popup_engineer_btn'),
 popup_engineer = document.querySelector('.popup_engineer'),
 btn_close = document.querySelector('.popup_engineer .popup_close'),
-form_end = document.getElementById('ku')
+popup = document.querySelector('.popup'),
+popup_close = document.querySelector('.popup .popup_close'),
+phone_link = document.getElementsByClassName('phone_link');
 
 popup_engineer_btn.addEventListener('click', () => {
 	popup_engineer.style.display = 'block';		
@@ -19,9 +21,72 @@ window.onclick = function(event) {
 	if (event.target == popup_engineer) {
 		popup_engineer.style.display = 'none'
 	}
+	if (event.target == popup) {
+		popup.style.display = 'none';
+	}
 } 
 
+for(var i = 0; i < phone_link.length; i++) {
+	phone_link[i].addEventListener('click', function() {
+		popup.style.display = 'block';
+	});
+}
+
+popup_close.addEventListener('click', function() {
+	popup.style.display = 'none';
+});
+
+
 //Формы в модыльных окнах
+
+let msg = new Object();
+msg.loading = "Идет отправка...";
+msg.success = "Отправлено";
+msg.failure = "Ошибка";
+
+let form_engineer = document.getElementById('form_engineer'),
+form_popup = document.getElementById('form_popup');
+statusmsg = document.createElement('div');
+
+function initPopupForm(elem_form) {
+
+	elem_form.addEventListener('submit', function(e) {		
+		e.preventDefault();		
+		elem_form.appendChild(statusmsg);
+		var formData = new FormData(elem_form);
+
+		function postData(data) {
+			var request = new XMLHttpRequest();
+			request.open('POST', 'server.php');
+			request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");		    
+
+			request.onreadystatechange = function() { 
+				if(request.readyState < 4) {
+					console.log(request);
+					statusmsg.innerHTML = msg.loading;
+				} else if (request.readyState === 4) {
+
+					if(request.status == 200 && request.status < 300) {
+						statusmsg.innerHTML = msg.success;					
+					} else {
+						statusmsg.innerHTML = msg.failure;
+					}
+				}
+			}
+			request.send(data);
+
+			for(let i = 0; i < input.length; i++) {
+				input[i].value = '';
+			}
+		}
+		postData(formData);
+	});
+}
+
+initPopupForm(form_engineer);
+initPopupForm(form_popup);
+
+//6 форм на странице
 
 let message = new Object();
 message.loading = "Идет отправка...";
