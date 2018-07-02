d597d8b3f9bcae7801d2ae521ecae6763eee13d4
unknown <jakushkolibero@gmail.com>
2018-07-02 20:22:45 +0200

    images-overlay

--- js/script.js
+++ js/script.js
@@ -480,8 +480,33 @@ function addZero(num){
 }
 
 
-//
+//Картинки при клике
 
+var works_link = document.querySelectorAll('#work a'),
+    divWork;
 
+for(var l = 0; l < works_link.length; l++) {
+	works_link[l].addEventListener('click', function(e) {
+		e.preventDefault();
+		console.log(this.getAttribute('href'));
+		var at = this.getAttribute('href');
+
+		divWork = document.createElement('div');
+		divWork.classList.add('overlay');
+		document.body.appendChild(divWork);
+
+		var imgBig = document.createElement('img');
+		imgBig.setAttribute('src', at);
+		imgBig.classList.add('imgStyle');
+		divWork.appendChild(imgBig);
+	})
+}
+
+window.onclick = function(event) {
+	if (event.target == divWork) {
+		divWork.style.display = 'none'
+	}
+	//console.log(divWork)	
+} 
 
 })
